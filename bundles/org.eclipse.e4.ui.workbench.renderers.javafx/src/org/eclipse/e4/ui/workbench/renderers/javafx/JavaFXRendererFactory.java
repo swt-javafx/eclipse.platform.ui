d111f625aa367a9f96d50a404d6bc794dd9b64a7
Doug Schaefer <dschaefer@qnx.com>
2014-01-01 20:56:37 -0500

    New JavaFX Renderers plugin.

---
+++ bundles/org.eclipse.e4.ui.workbench.renderers.javafx/src/org/eclipse/e4/ui/workbench/renderers/javafx/JavaFXRendererFactory.java
@@ -0,0 +1,16 @@
+package org.eclipse.e4.ui.workbench.renderers.javafx;
+
+import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
+import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.workbench.swt.factories.IRendererFactory;
+
+@SuppressWarnings("restriction")
+public class JavaFXRendererFactory implements IRendererFactory {
+
+	@Override
+	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
+
+		return null;
+	}
+	
+}
