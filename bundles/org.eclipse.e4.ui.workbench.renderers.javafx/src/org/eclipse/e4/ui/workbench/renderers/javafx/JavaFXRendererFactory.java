0a57629ef32730b9473d3a4221a2f569932fae6b
Doug Schaefer <dschaefer@qnx.com>
2014-01-03 13:19:53 -0500

    More work on JavaFX renderer.

--- bundles/org.eclipse.e4.ui.workbench.renderers.javafx/src/org/eclipse/e4/ui/workbench/renderers/javafx/JavaFXRendererFactory.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.javafx/src/org/eclipse/e4/ui/workbench/renderers/javafx/JavaFXRendererFactory.java
@@ -2,6 +2,7 @@ package org.eclipse.e4.ui.workbench.renderers.javafx;
 
 import org.eclipse.e4.ui.internal.workbench.swt.AbstractPartRenderer;
 import org.eclipse.e4.ui.model.application.ui.MUIElement;
+import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
 import org.eclipse.e4.ui.workbench.swt.factories.IRendererFactory;
 
 @SuppressWarnings("restriction")
@@ -9,7 +10,8 @@ public class JavaFXRendererFactory implements IRendererFactory {
 
 	@Override
 	public AbstractPartRenderer getRenderer(MUIElement uiElement, Object parent) {
-
+		if (uiElement instanceof MWindow)
+			return new WindowRenderer();
 		return null;
 	}
 	
