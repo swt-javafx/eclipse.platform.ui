cebd03d6a6a83bd60641bed9d6304a0496d84b72
Doug Schaefer <dschaefer@qnx.com>
2014-01-05 17:21:57 -0500

    Pull back the Renderers.

--- bundles/org.eclipse.e4.ui.workbench.renderers.javafx/src/org/eclipse/e4/ui/workbench/renderers/javafx/JavaFXPresentationEngine.java
+++ bundles/org.eclipse.e4.ui.workbench.renderers.javafx/src/org/eclipse/e4/ui/workbench/renderers/javafx/JavaFXPresentationEngine.java
@@ -1,28 +1,31 @@
 package org.eclipse.e4.ui.workbench.renderers.javafx;
 
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import javax.inject.Inject;
+import javax.inject.Named;
+
+import org.eclipse.core.databinding.observable.Realm;
 import org.eclipse.e4.core.contexts.IEclipseContext;
+import org.eclipse.e4.core.di.annotations.Optional;
+import org.eclipse.e4.ui.internal.workbench.E4Workbench;
+import org.eclipse.e4.ui.internal.workbench.swt.PartRenderingEngine;
+import org.eclipse.e4.ui.internal.workbench.swt.ResourceUtility;
 import org.eclipse.e4.ui.model.application.MApplication;
 import org.eclipse.e4.ui.model.application.MApplicationElement;
-import org.eclipse.e4.ui.model.application.ui.MUIElement;
 import org.eclipse.e4.ui.model.application.ui.basic.MWindow;
-import org.eclipse.e4.ui.workbench.IPresentationEngine;
+import org.eclipse.e4.ui.workbench.IResourceUtilities;
 import org.eclipse.equinox.app.IApplication;
+import org.eclipse.jface.databinding.swt.SWTObservables;
 import org.eclipse.swt.widgets.Display;
 
-public class JavaFXPresentationEngine implements IPresentationEngine {
+@SuppressWarnings("restriction")
+public class JavaFXPresentationEngine extends PartRenderingEngine {
 
 	static JavaFXPresentationEngine instance;
 	private MApplicationElement uiRoot;
-	private IEclipseContext appContext;
-	private Stage primaryStage;
+	private IEclipseContext runContext;
 	
 	public static final class App extends Application {
 		@Override
@@ -30,74 +33,50 @@ public class JavaFXPresentationEngine implements IPresentationEngine {
 			instance.doStart(primaryStage);
 		}
 	}
-	
-	@Override
-	public Object createGui(MUIElement element) {
-		if (element instanceof MWindow) {
-			MWindow window = (MWindow)element;
-	        primaryStage.setTitle(window.getLabel());
-	        
-	        Button btn = new Button();
-	        btn.setText("Say 'Hello World'");
-	        btn.setOnAction(new EventHandler<ActionEvent>() {
-	 
-	            @Override
-	            public void handle(ActionEvent event) {
-	                System.out.println("Hello World!");
-	            }
-	        });
-	        
-	        StackPane root = new StackPane();
-	        root.getChildren().add(btn);
-	        primaryStage.setScene(new Scene(root, 300, 250));
-	        primaryStage.show();
-		}
-		return null;
+
+	@Inject
+	public JavaFXPresentationEngine(
+			@Named(E4Workbench.RENDERER_FACTORY_URI) @Optional String factoryUrl) {
+		super(factoryUrl);
 	}
 
 	@Override
-	public Object createGui(MUIElement element, Object parentWidget, IEclipseContext parentContext) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-	
-	@Override
-	public void removeGui(MUIElement element) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public void focusGui(MUIElement element) {
-		// TODO Auto-generated method stub
-
-	}
-
-	@Override
-	public Object run(MApplicationElement uiRoot, IEclipseContext appContext) {
+	public Object run(MApplicationElement uiRoot, final IEclipseContext runContext) {
 		instance = this;
 		this.uiRoot = uiRoot;
-		this.appContext = appContext;
+		this.runContext = runContext;
 		App.launch(App.class, new String[0]);
 		return IApplication.EXIT_OK;
 	}
 	
 	private void doStart(Stage primaryStage) {
-		Display.running = true;
-		this.primaryStage = primaryStage;
-		
-		if (uiRoot instanceof MApplication) {
-			MApplication app = (MApplication)uiRoot;
-			for (MWindow window : app.getChildren()) {
-				createGui(window);
-			}
+		final Display display;
+		if (runContext.get(Display.class) != null) {
+			display = runContext.get(Display.class);
+		} else {
+			display = Display.getDefault();
+			runContext.set(Display.class, display);
 		}
+
+		Realm.runWithDefault(SWTObservables.getRealm(display), new Runnable() {
+			public void run() {
+				initializeStyling(display, runContext);
+
+				Display.running = true;
+				Display.primaryStage = primaryStage;
+				
+				// Register an SWT resource handler
+				runContext.set(IResourceUtilities.class.getName(),
+						new ResourceUtility());
+		
+				if (uiRoot instanceof MApplication) {
+					MApplication app = (MApplication)uiRoot;
+					for (MWindow window : app.getChildren()) {
+						createGui(window);
+					}
+				}
+			}
+		});
 	}
 	
-	@Override
-	public void stop() {
-		// TODO Auto-generated method stub
-
-	}
-
 }
